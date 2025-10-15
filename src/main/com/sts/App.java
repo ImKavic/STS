e2f8b1f32751d66ed90433d0235e1e3f95a4ea81
Kavic <f.avishena@gmail.com>
2025-10-15 23:56:39 +0700

    feat: remove unused variable by making if its not null, close the connection

--- src/main/com/sts/App.java
+++ src/main/com/sts/App.java
@@ -6,6 +6,8 @@ import java.sql.Connection;
 public class App {
     public static void main(String[] args) {
         Connection conn = DatabaseConnection.getConnection();
-        DatabaseConnection.closeConnection();
+        if (conn != null) {
+            DatabaseConnection.closeConnection();
+        }
     }
 }
