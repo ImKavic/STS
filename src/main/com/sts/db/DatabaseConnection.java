453316b1ad18e1c0535db34f7964d0747f3f7a6c
Kavic <f.avishena@gmail.com>
2025-10-15 23:53:06 +0700

    feat: gitignore, launch and task json, java main app connect database separate file

---
+++ src/main/com/sts/db/DatabaseConnection.java
@@ -0,0 +1,39 @@
+package com.sts.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static final String URL = "jdbc:sqlserver://Kavic;databaseName=STS;encrypt=false;trustServerCertificate=true;";
+    private static final String USER = "sa";
+    private static final String PASSWORD = "as";
+
+    private static Connection connection;
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+                System.out.println("Koneksi ke database berhasil!");
+            } catch (SQLException e) {
+                System.out.println("Gagal terhubung ke database.");
+                // e.printStackTrace();
+            }
+        }
+        return connection;
+    }
+
+    public static void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+                connection = null;
+                System.out.println("Koneksi database ditutup.");
+            } catch (SQLException e) {
+                // e.printStackTrace();
+            }
+        }
+    }
+}
