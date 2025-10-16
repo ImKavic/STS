29657caea345e4184a6f0d1b9c07eadc7d3cb6f5
Fikri Avishena <95864463+ImKavic@users.noreply.github.com>
2025-10-16 20:46:28 +0700

    Refactor User class fields to follow naming conventions

--- src/main/com/sts/model/User.java
+++ src/main/com/sts/model/User.java
@@ -1,7 +1,8 @@
 package com.sts.model;
 
 public class User {
-    public long id;
-    public String username;
-    public String password;
+    public long  Id;
+    public String Username;
+    public String Password;
+    public string Name;
 }
